e884d062515b5a335f0b338224b7309a7ba45ffa
Vishal ahir <vishalahir0602@gmail.com>
2025-08-04 21:19:14 +0530

    Add the frontend deploy

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "http://localhost:8080"
+export const BASE_URL = "https://proconnect-linkedin-clone-61mb.onrender.com"
 
 export const clientServer = axios.create({
     baseURL : BASE_URL,
